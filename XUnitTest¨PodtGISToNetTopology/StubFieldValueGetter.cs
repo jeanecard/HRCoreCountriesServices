3bf7539777e34b40aaa10170c4103a6b1d7b841a
ECARD <jean.ecard@outlook.fr>
2019-04-28 20:13:11 +0200

    Add UnitTest Project in solution (previously fail)

---
+++ XUnitTest¨PodtGISToNetTopology/StubFieldValueGetter.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql.LegacyPostgis;
+using Npgsql.PostgresTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tools.Interfaces;
+
+namespace XUnitPostGis
+{
+    class StubFieldValueGetter : IFieldValueGetter
+    {
+        public int FieldCount { get; set; }
+
+        public T GetFieldValue<T>(int ordinal) 
+        {
+            if (ordinal == OrdinalToStub)
+            {
+                return (T)(Object)GeometryToStub;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        public PostgresType GetPostgresType(int ordinal)
+        {
+            if (ordinal == OrdinalToStub)
+            {
+                
+                return new PostgresTypeStub();
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        public PostgisGeometry GeometryToStub{get; set;}
+        public int OrdinalToStub { get; set; }
+
+    }
+}
