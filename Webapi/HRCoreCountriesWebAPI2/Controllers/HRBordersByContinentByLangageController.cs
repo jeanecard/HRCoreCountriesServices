24d71e2b3f19ae7e09c191dbac5a1d276fdd8867
ECARD <jean.ecard@outlook.fr>
2019-11-01 16:44:20 +0100

    Init filters on HRBorders

---
+++ Webapi/HRCoreCountriesWebAPI2/Controllers/HRBordersByContinentByLangageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRCoreBordersModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRBordersAndCountriesWebAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HRBordersByContinentByLangageController : ControllerBase
+    {
+        // GET: api/HRCountriesByContientByLangage/{continentID}/{LanguageID}
+        /// <summary>
+        /// Get All countries of a specific Region for a specific Language
+        /// </summary>
+        /// <param name="continentId">a Continent id : All, Africa, Americas, Asia, Empty, Europe, Oceania, Polar</param>
+        /// <param name="langageId">a iso language code (iso6391 Or Iso6392 eg : fr for France (no case sensitive))</param>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("{continentId}/{langageId}")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HRBorder>>> GetHRBordersByContinentByLangageIDAsync(String continentId, String langageId)
+        {
+            await Task.Delay(1);
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+    }
+}
---
+++ Webapi/HRCoreCountriesWebAPI2/Controllers/HRBordersByContinentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRCoreBordersModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRBordersAndCountriesWebAPI2.Controllers
+{
+    /// <summary>
+    /// Class to manage HRBorders via Region.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HRBordersByContinentController : ControllerBase
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">a Continent id : All, Africa, Americas, Asia, Empty, Europe, Oceania, Polar</param>
+        /// <returns>HRBorder with continent id</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<HRBorder>>> GetHRBordersByContinentIDAsync(String id)
+        {
+            await Task.Delay(1);
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+    }
+}
